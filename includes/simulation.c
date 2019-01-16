39d560b5eb9633e77485114897d1bb438a225954
Kavun <kavunnuggihalli@gmail.com>
2019-01-16 11:33:56 -0500

    mo

---
+++ includes/simulation.c
@@ -0,0 +1,9 @@
+//Flip requires user to type exit
+int quit = 0;
+
+//Parsing is built into the event loop
+while(quit == 0){
+
+
+
+}
--- index.c
+++ index.c
@@ -1,24 +1,156 @@
-//COMPILER BASED INCLUDES
+/*
+Kavun Nuggihalli
+Feb 12 2018
+Low Level Ajaj
+Hw 3
+
+This is an ATM program.
+Welcome funcitons just prints out hello
+Pin function uses pointer and for loop to let user try 3 times
+if pin is returned correctly options function prints what user can do
+sorrounded with while loop for menu
+logic inside adds to transictions and affects local vars
+exit chooses recipt
+
+*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-//CREATE STRUCTURES FOR LINKED LISTS AND PROCESS SIMULATION
-#include "includes/structures.c"
-//IMPLEMENTS FUNCTION CALLS AND STATES FOR APPLICATION
-#include "includes/global.c"
+//functions
+void pr(char s[], float x);
+void welcome();
+void options();
+void pin(float *pin);
+void balance();
 
-//MAIN FUNCTION
-int main(int argc, char const *argv[]) {
+//MAIN
+int main(){
+  //vars
+  float p = 0;//bit flip for pin
+  float b = 5000;//balance
+  float numt = 0;// number transactions
+  float recipt = 0;//recipt
+  float val; // what user does
+  float limmit = 1000;
+  float dlimmit = 1000;
+  float take,give,leave; //Withdrawl amount
 
-  //VARIABLES FROM CONFIG FILE WILL BE SAVED TO VARIABLE ARR[]
-  //THERE ARE 12 VARIABLES FROM CRONFIG SO HENCE 12 ARR VALUES
-  #include "includes/config.c"
+  welcome();//Start
+  pin(&p);//Get pin and all attempts
 
-  //RUN SIMULATION
-  #include "includes/simulation.c"
+  if(p == 0){pr("Fail",0);}else{//The user will either enter or fail
+    int x = 0;
+    while(x<1){
+      options();//Passed now what?
+      printf("What will you do [enter a number]:");
+      scanf("%f", &val);
+
+      if(val == 1){//balance
+        printf("\n");
+        printf("Balance: $%.1f\n", b);
+        numt++;
+      }else if(val == 2){//Withdrawl
+        printf("\n");
+        printf("Choose $20,$40,$60,$80,$100... [$%.1f limmit]: \n", limmit);
+        printf("How much will you withdraw [enter an amount]:");
+        scanf("%f", &take);
+
+        if(limmit > 0 && (int)take%20 == 0 && limmit>take){
+          limmit = limmit - take;
+          b = b - take;
+          float output = take/20;
+          printf("\n");
+          printf("Balance: $%.1f\n", b);
+          printf("Limmit: $%.1f\n", limmit);
+          printf("Output: %.0f $20 bills \n", output);
+          numt++;
+        }else{
+          printf("You have either hit your limmit for today or have given an invalid input \n");
+          printf("Remember all inputs should be an increment of $20 \n");
+          numt++;
+        }
+      }else if(val == 3){//deposit
+        printf("\n");
+        printf("Deposit [$%.1f limmit]: \n", dlimmit);
+        printf("How much will you deposit [enter an amount]:");
+        scanf("%f", &give);
+
+        if(dlimmit > 0 && dlimmit>give){
+          dlimmit = dlimmit - give;
+          b = b + give;
+          printf("\n");
+          printf("Balance: $%.1f\n", b);
+          printf("Limmit: $%.1f\n", dlimmit);
+          numt++;
+        }else{
+          printf("You have hit your limmit \n");
+          numt++;
+        }
+      }else if(val == 4){//exit
+        printf("\n");
+        printf("Exit");
+        printf("Recipt? [1=yes 2=no]:");
+        scanf("%f", &leave);
+
+        if(leave == 1){
+          printf("\n");
+          pr("Thank you have a good day.", 0);
+          printf("Balance: $%.1f\n", b);
+          printf("Withdrawl Limmit: $%.1f\n", limmit);
+          printf("Deposit Limmit: $%.1f\n", dlimmit);
+          printf("Transactions: %.0f\n", numt);
+          printf("\n");
+          x = 2;
+        }else{
+          pr("Thank you have a good day.", 0);
+          x = 2;
+        }
+      }else{
+        pr("Not a valid input try again", 0);
+      }
+    }
+
+  }
 
   return 0;
 }
-//ACTUAL FUNCTION CODE
-#include "includes/functions.c"
+
+//Generic Function to print UI for string
+void pr(char s[], float x) {
+  if(x > 0){
+    printf("%s: %.1f \n", s,x);
+  }else if(x == -1){
+    printf("=================================\n");
+  }else{
+    printf("%s \n", s);
+  }
+}
+
+//try all pins from user
+void pin(float *pin){
+    *pin = 1;
+}
+
+//welcome
+void welcome(){
+  printf("Welcome to \"Your Bank of Choice\" \n");
+  printf("Please enter your pin below \n");
+  printf("=================================\n");
+}
+
+//Balance
+void balance(){
+
+}
+
+//options
+void options(){
+  printf("\n");
+  pr("-",-1);
+  pr("The types of ATM transictions are:", 0);
+  printf("\n");
+  pr("\t 1.Balance", 0);
+  pr("\t 2.Withdrawl", 0);
+  pr("\t 3.Deposit", 0);
+  pr("\t 4.Exit", 0);
+  printf("\n");
+}
